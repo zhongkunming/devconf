16244a1a1618852974035f0fd106103663c9f278
zhongkunming <73384930+zhongkunming@users.noreply.github.com>
2025-07-14 14:05:35 +0800

    feat(supervisor): 新增 Supervisor 应用管理脚本和配置
    - 添加 .gitignore 文件，忽略 Visual Studio Code 和 JetBrains IDEs 的配置文件
    - 新增 supervisorctl.sh 脚本，用于管理 Supervisor 应用的启动、停止、部署等操作
    - 新增 supervisorweb.conf 配置文件，用于 Nginx 服务器配置
    - 新增 supervisorweb.sh脚本，用于安装和配置 Node.js 环境

---
+++ supervisor/supervisorweb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export NVM_DIR="$JENKINS_HOME/software/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+nvm use --lts
+npm install -g pnpm --registry http://192.168.10.10:30380/repository/npm-public
+pnpm install --registry http://192.168.10.10:30380/repository/npm-public
+pnpm build
