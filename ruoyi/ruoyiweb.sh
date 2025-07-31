3b21123c723671083c19c32ccd281af992a47f46
zhongkunming <unknownskak@gmail.com>
2025-07-31 12:04:37 +0800

    build(ruoyi): 使用 pnpm 替代 yarn 进行包管理
    - 安装 pnpm 替代 yarn
    - 使用 pnpm 安装 sortablejs依赖
    - 使用 pnpm 进行生产构建
    - 更新构建脚本以适应新的包管理工具

--- ruoyi/ruoyiweb.sh
+++ ruoyi/ruoyiweb.sh
@@ -3,7 +3,8 @@ export NVM_DIR="$JENKINS_HOME/software/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 nvm use --lts
-npm install -g yarn --registry http://192.168.10.10:30380/repository/npm-registry-public/
-yarn --registry http://192.168.10.10:30380/repository/npm-registry-public/
-yarn build:prod
+npm install -g pnpm --registry http://192.168.10.10:30380/repository/npm-registry-public/
+pnpm install sortablejs
+pnpm install --registry http://192.168.10.10:30380/repository/npm-registry-public/
+pnpm build
 tar -czf dist.tar.gz dist
