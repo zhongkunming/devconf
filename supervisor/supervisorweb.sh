d9f3c44e5a96f144b054e0b598e9f455c6112c4a
zhongkunming <unknownskak@gmail.com>
2025-08-02 18:18:10 +0800

    build(supervisor): 使用 yarn替代 pnpm 进行包管理
    - 安装全局 yarn 代替 pnpm
    - 使用 yarn 进行项目依赖安装和构建
    - 保持了原有的 tar压缩 dist 目录的逻辑

--- supervisor/supervisorweb.sh
+++ supervisor/supervisorweb.sh
@@ -3,7 +3,7 @@ export NVM_DIR="$JENKINS_HOME/software/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 nvm use --lts
-npm install -g pnpm --registry http://192.168.10.10:30380/repository/npm-registry-public/
-pnpm install --registry http://192.168.10.10:30380/repository/npm-registry-public/
-pnpm build
+npm install -g yarn --registry http://192.168.10.10:30380/repository/npm-registry-public/
+yarn install --registry http://192.168.10.10:30380/repository/npm-registry-public/
+yarn build
 tar -czf dist.tar.gz dist
